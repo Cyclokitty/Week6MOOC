c908621dc6d5f339806130509001344195f30f5f
Laura Vingada <laura@vingada.com>
2017-09-14 20:09:06 -0400

    Practicing arrays

--- src/com/cyclokitty/Main.java
+++ src/com/cyclokitty/Main.java
@@ -1,8 +1,29 @@
 package com.cyclokitty;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int[] nums = {100, 1, 42};
+        System.out.println(Arrays.toString(nums));
+
+        System.out.println();
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println((i + 1) + ". " + nums[i]);
+        }
+
+        System.out.println();
+
+        System.out.println(sum(new int[] {5, 1, 3, 4, 2}));
+    }
+
+    public static int sum(int[] arr) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return total;
     }
 }
